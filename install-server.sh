2883ae083b33b3ff9bfd5b2f677af01479bf0ac4
chenjunlin <3397920970@qq.com>
2021-09-29 15:57:33 +0800

    push

--- install-server.sh
+++ install-server.sh
@@ -2,15 +2,10 @@ sudo apt install wget git tar -y
 
 echo "开始安装服务"
 
-sudo wget -P /etc/systemd/system/ https://cdn.jsdelivr.net/gh/tanzi1997/kitten@master/kitten-serve.service -O kitten-serve.service
+sudo curl -LJo /etc/systemd/system/kitten-server.service https://cdn.jsdelivr.net/gh/tanzi1997/kitten@master/kitten-server.service
 
-sudo wget -P /usr/local/bin/ https://github.com/tanzi1997/kitten/releases/download/0.1/kitten-server -O kitten-server
+sudo curl -LJo /usr/local/bin/kitten-server https://github.com/tanzi1997/kitten/releases/download/0.1/kitten-server
 
+echo "服务启动"
 
-echo "开始安装程序"
-
-sudo chmod -R 777 ./kitten-server
-
-echo "程序启动"
-
-service kitten-serve start
+service kitten-server start
